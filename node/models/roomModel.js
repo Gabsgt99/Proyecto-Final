4e054bc61fea0bccbdeb96abf05b2ef3c3c4aee0
Akemi Noriega <ake.sudestada@gmail.com>
2023-06-29 13:26:19 +0200

    add roomModel

---
+++ node/models/roomModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const roomModelSchema = new mongoose.Schema({
+  
+    name: {
+      type: String,
+      required: true,
+    },
+    aforo: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+
+},{ timestamps: true }
+);
+
+export default mongoose.model("roomModel", roomModelSchema);
