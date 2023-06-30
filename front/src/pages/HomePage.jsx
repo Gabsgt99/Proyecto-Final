c87dd4b2e2de92aa3b296c0a3e3feaea8212965e
Rocio <red200921@gmail.com>
2023-06-30 14:22:04 +0200

    Realizar cambios en HomePage.jsx

--- front/src/pages/HomePage.jsx
+++ front/src/pages/HomePage.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import Layout from '../components/layout/layout';
 
 
 function HomePage() {
   return ( <>
-        HOME
+        <Layout/>
           </> );      
 };
 
