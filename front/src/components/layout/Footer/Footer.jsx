8e6c19619787cfe751eaf8ae62b9edc18b209cbb
Rocio <red200921@gmail.com>
2023-07-07 11:05:03 +0200

    cambio de logo footer

--- front/src/components/layout/Footer/Footer.jsx
+++ front/src/components/layout/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './img/logof5.png';
+import logo from './logof5.png';
 import './Footer.css'
 import {Link} from 'react-router-dom'
 
--- front/src/pages/HomePage.jsx
+++ front/src/pages/HomePage.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import Layout from '../components/layout/layout';
+import Layout from '../components/Layout/Layout.jsx';
 
-
-function HomePage() {
+const HomePage = () => {
   return ( <>
         <Layout/>
           </> );      
