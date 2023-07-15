5f7340c7dcbb653f062be0cde7f3f7eeb3a2bbf2
Sergio Guzmán <126780505+Sergio-Guzman@users.noreply.github.com>
2023-07-15 10:53:13 +0200

    agegado en HomePage el codigo para que sea dinamica revisarla y si le faltan datos son de db o rutas no lo tengo muy claro

--- front/src/pages/HomePage.jsx
+++ front/src/pages/HomePage.jsx
@@ -1,14 +1,84 @@
-import React from 'react';
-import Layout from '../components/Layout/Layout';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Mongo from "react-mongo";
 
+function HomePage (){
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [],
+      text: "",
+      links: []
+    };
+  }
 
-const HomePage = () => {
-  return ( <>
-  
-        <Layout/>
-      
-          </> );      
-};
+  componentDidMount() {
+    // Carga los datos de la base de datos.
+    Mongo.find(
+      {
+        // La consulta para encontrar los datos en la base de datos.
+      },
+      (err, results) => {
+        if (err) {
+          // Maneja el error.
+        } else {
+          // Actualiza el estado con los datos de la base de datos.
+          this.setState({
+            images: results.map(image => image.image),
+            text: results.map(image => image.text),
+            links: results.map(image => image.link)
+          });
+        }
+      }
+    );
+  }
 
+  render() {
+    // Renderiza los datos en el componente.
+    return (
+      <div>
+        <h1>Mi aplicación</h1>
+        <div>
+          {this.state.images.map(image => (
+            <img src={image} alt={image} />
+          ))}
+        </div>
+        <div>
+          {this.state.text}
+        </div>
+        <div>
+          {this.state.links.map(link => (
+            <a href={link}>{link}</a>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  onScroll = () => {
+    // Carga más datos de la base de datos cuando el usuario hace scroll.
+    Mongo.find(
+      {
+        // La consulta para encontrar los datos en la base de datos.
+      },
+      (err, results) => {
+        if (err) {
+          // Maneja el error.
+        } else {
+          // Actualiza el estado con los datos de la base de datos.
+          this.setState({
+            images: this.state.images.concat(results.map(image => image.image)),
+            text: this.state.text.concat(results.map(image => image.text)),
+            links: this.state.links.concat(results.map(image => image.link))
+          });
+        }
+      }
+    );
+  };
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+}
 export default HomePage
