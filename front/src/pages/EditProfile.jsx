ee97c3497c1d92d663e2db7a1c2941f45ce60950
Rocio <red200921@gmail.com>
2023-07-17 10:39:42 +0200

    Finished page record

--- front/src/pages/EditProfile.jsx
+++ front/src/pages/EditProfile.jsx
@@ -1,62 +1,67 @@
-import React, { useState } from 'react';
-import '../../src/index.css';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "../../src/index.css";
 
 const EditProfile = () => {
-  const [name, setName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [isActive, setActive] = useState(null);
 
   const handleSaveChanges = () => {
-    console.log('Guardando cambios:', { name, lastName, email });
+    console.log("Guardando cambios:", { name, lastName, email });
   };
 
   return (
     <div className="container">
-      <div className="link">
-        <Link to="/">Administrador</Link> 
-         <Link to="/">Cerrar Sesión</Link>
-      </div>
-      <h1 className='editar'>Editar perfil</h1>
+      <h1 className="editar">Editar perfil</h1>
       <div className="form-container">
-      <form>
-        <div className="input-container">
-          <label>Nombre:</label>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
+        <form>
+          <div className="input-container">
+            <label>Nombres:</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
 
-        <div className="input-container">
-          <label>Apellidos:</label>
-          <input
-            type="text"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </div>
+          <div className="input-container">
+            <label>Apellidos:</label>
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
 
-        <div className="input-container">
-          <label>Correo electrónico:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-      </form>
-    </div>
-      <div className="buttons-container">
-         <button type="button" className="cancel-button">
-            Cancelar
-          </button>
-            <button type="button" onClick={handleSaveChanges} className="save-button">
-            Guardar cambios
-          </button>
-       </div>
-
+          <div className="input-container">
+            <label>Correo electrónico:</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+        </form>
+      </div>
+      <div className="button-container">
+        <button
+          type="submit"
+          className={`btn btn-register ${
+            isActive === "cancel" ? "active" : ""
+          }`}
+          onClick={() => setActive("cancel")}
+        >
+          Cancelar
+        </button>
+        <button
+          type="submit"
+          className={`btn btn-register ${isActive === "save" ? "active" : ""}`}
+          onClick={() => setActive("save")}
+        >
+          Guardar
+        </button>
+      </div>
     </div>
   );
 };
--- front/src/pages/RegisterPage.jsx
+++ front/src/pages/RegisterPage.jsx
@@ -1,77 +1,45 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-
- 
 function RegisterPage() {
-  /* const [formData, setFormData] = useState({
-    name: '',
-    lastname: '',
-    email: '',
-    isAdmin: false,
-  });
-   */
-
   const [name, setName] = useState("");
   const [lastname, setLastname] = useState("");
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [admi, setAdmi] = useState(false); // Cambiado a un valor booleano
+  const [isActive, setActive] = useState(null);
   const navigate = useNavigate();
-
-  /* const [modalVisible, setModalVisible] = useState(false);
-  const [successMessage, setSuccessMessage] = useState(''); */
-
-  /* const handleChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    const fieldValue = type === 'checkbox' ? checked : value;
-
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: fieldValue,
-    }));
-  }; */
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const res = await axios.post("/api/v1/auth/register", {
         name,
-	      lastname,
+        lastname,
         email,
-        password
+        admi,
       });
       if (res && res.data.success) {
         /* alert.success(res.data && res.data.message); */ //<===aqui va un mensaje popup de exito en el registro ======
         navigate("/"); //Aqui poner a donde va despues del registro==========
       } else {
-     //<===aqui va un mensaje popup
-     console.log(name,lastname,email,password)
+        //<===aqui va un mensaje popup
+        console.log(name, lastname, email, admi);
       }
     } catch (error) {
       console.log(error);
       //<===aqui va un mensaje popup
     }
-
-
-
-    // Aquí puedes realizar acciones con los datos del formulario, como enviarlos a un servidor
-    /* setSuccessMessage('Ha registrado correctamente al usuario');
-    setModalVisible(true); */
   };
-
-  /* const closeModal = () => {
-    setModalVisible(false);
-    setSuccessMessage('');
-  }; */
 
   return (
     <div className="container-fluid">
       <div className="row justify-content-center">
         <div className="col-md-6 mt-5 px-5 ">
-          <form onSubmit={handleSubmit} >
+          <h1 className="editar">Registrar nuevo usuario</h1>
+          <form onSubmit={handleSubmit}>
             <div className="form-group py-3">
-              <label htmlFor="firstName">Nombre</label>
+              <label htmlFor="firstName">Nombres:</label>
               <input
                 type="text"
                 className="form-control"
@@ -84,7 +52,7 @@ function RegisterPage() {
               />
             </div>
             <div className="form-group py-3">
-              <label htmlFor="lastName">Apellido</label>
+              <label htmlFor="lastName">Apellidos:</label>
               <input
                 type="text"
                 className="form-control"
@@ -97,7 +65,7 @@ function RegisterPage() {
               />
             </div>
             <div className="form-group py-3">
-              <label htmlFor="email">Email</label>
+              <label htmlFor="email">Email:</label>
               <input
                 type="email"
                 className="form-control"
@@ -109,57 +77,40 @@ function RegisterPage() {
                 autoFocus
               />
             </div>
-            <div className="form-group py-3">
-              <label htmlFor="password">Contraseña</label>
-              <input
-                type="password"
-                className="form-control"
-                id="password"
-                name="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                autoFocus
-              />
-            </div>
-           {/*  <div className="form-group form-check py-3">
+            <div>
+              <label style={{ marginRight: "5px"}}>
+                Admi:
+              </label>
               <input
                 type="checkbox"
-                className="form-check-input"
-                id="isAdmin"
-                name="isAdmin"
-                checked={isAdmin}
-                onChange={handleChange}
+                name="admi"
+                checked={admi}
+                onChange={(e) => setAdmi(e.target.checked)}
               />
-              <label className="form-check-label" htmlFor="isAdmin">
-                ¿Es Admin?
-              </label>
-            </div> */}
-            <button type="submit" className="btn btn-register">
-              Registrarse
-            </button>
+            </div>
+            <div className="button-container">
+              <button
+                type="submit"
+                className={`btn btn-register ${
+                  isActive === "cancel" ? "active" : ""
+                }`}
+                onClick={() => setActive("cancel")}
+              >
+                Cancelar
+              </button>
+              <button
+                type="submit"
+                className={`btn btn-register ${
+                  isActive === "save" ? "active" : ""
+                }`}
+                onClick={() => setActive("save")}
+              >
+                Guardar
+              </button>
+            </div>
           </form>
         </div>
       </div>
-
-      {/* Modal de éxito */}
-      {/* {modalVisible && (
-        <div className="modal" tabIndex="-1" role="dialog">
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">Éxito</h5>
-                <button type="button" className="close" onClick={closeModal}>
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                <p>{successMessage}</p>
-              </div>
-            </div>
-          </div>
-        </div> */}
-      
     </div>
   );
 }
