f6a2e10a9cac3c0631ea091b8d20b26fbd653fd3
Rocio <red200921@gmail.com>
2023-07-25 14:55:06 +0200

    xxxx

--- front/src/components/layout/layout.jsx
+++ front/src/components/layout/layout.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import React from "react";
+import { Header } from "./Header/Header.jsx";
+import Footer from "./Footer/Footer.jsx";
+import Navbar from "./Navbar/Navbar.jsx";
 
-const Layout = ({ children,title,description,keywords,author }) => {
-    return(
-        <div>
-            <Header/>
-            <main style={{minHeight:'70vh'}}>
-            {children}</main>
-            <Footer/>
-        </div>
-    )
+export const Layout = ({ children, title, description, keywords, author }) => {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <main style={{ minHeight: "70vh" }}>{children}</main>
+      <Footer />
+    </div>
+  );
 };
 
 Layout.defaultProps = {
-    title:'Gestion Salas',
-    description:'Reservas Salas Equipo de trabajo',
-    keywords:'mern, react, nodejs,mongodb',
-    author:'Equipo Naranja',
-}
-
-export default Layout;
+  title: "Gestion Salas",
+  description: "Reservas Salas Equipo de trabajo",
+  keywords: "mern, react, nodejs,mongodb",
+  author: "Equipo Naranja",
+};
