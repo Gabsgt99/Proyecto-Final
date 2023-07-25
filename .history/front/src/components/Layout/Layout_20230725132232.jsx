84133553a68579062aec36a0a42d00a49a900514
Akemi Noriega <ake.sudestada@gmail.com>
2023-07-25 13:33:57 +0200

    Updated Layout & BurguerButton

---
+++ .history/front/src/components/Layout/Layout_20230725132232.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+
+const Layout = ({ children, title, description, keywords, author }) => {
+    return (
+      <div>
+        <Header />
+      <main style={{ minHeight: "70vh" }}>
+{children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+Layout.defaultProps = {
+  title: "Gestión de Salas Factoria F5",
+  description: "mern stack project",
+  keywords: "",
+  author: "Equipo Naranja",
+};
+
+export default Layout;
