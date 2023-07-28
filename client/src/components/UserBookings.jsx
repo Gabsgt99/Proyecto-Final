2c4f6cfac3c4340b04607c2451b1a5ab6abbcbe3
Gabsgt99 <gabrielaguzmant99@gmail.com>
2023-07-28 09:24:43 +0200

    userBookings page

--- client/src/components/UserBookings.jsx
+++ client/src/components/UserBookings.jsx
@@ -10,13 +10,22 @@ const UserBookings = () => {
       const response = await axios.get(`/api/v1/bookings/get-bookingsbyuser`);
       const bookings = response.data.bookings;
       
-      const formattedEvents = bookings.map((booking) => ({
-        id: booking._id,
-        title: 'Reservado',
-        start: moment(booking.start_date).format('YYYY-MM-DDTHH:mm'),
-        end: moment(booking.end_date).format('YYYY-MM-DDTHH:mm'),
-        allDay: false
-      }));
+      const formattedEvents = await Promise.all(
+        bookings.map(async (booking) => {
+          const userResponse = await axios.get(`/api/v1/auth/${booking._id}`);
+          const roomResponse = await axios.get(`/api/v1/rooms/${booking._id}`);
+
+          return {
+            id: booking._id,
+            title: userResponse.data.name, // Assuming the user name property is "name"
+            roomId: roomResponse.data.name, // Assuming the room name property is "name"
+            start: moment(booking.start_date).format('YYYY-MM-DDTHH:mm'),
+            end: moment(booking.end_date).format('YYYY-MM-DDTHH:mm'),
+            allDay: false
+          };
+        })
+      );
+
       setUserBookings(formattedEvents);
     } catch (error) {
       console.error(error);
